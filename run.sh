86253b59a42d72ed49a8d486c3a3837bbec462ad
Ricardo Job <sousajob@gmail.com>
2017-06-05 08:09:35 -0300

    exemplo em aula

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-mvn clean install
-docker build -t ricardojob/webapp . 
-docker run -p 8080:8080 -d ricardojob/webapp
+cd app && mvn clean install && cd ..
+
+docker build -t ricardojob/banco ./postgres
+docker build -t ricardojob/webapp ./app
+
+docker run -p 5433:5432 --name banco -d ricardojob/banco  
+docker run -p 8081:8080 -d --name app --link banco:host-banco ricardojob/webapp
