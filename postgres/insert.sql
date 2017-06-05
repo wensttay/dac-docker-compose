b4b424c478a6e0a5aaad23eed54f3379040f0aea
Ricardo Job <sousajob@gmail.com>
2017-06-05 10:19:17 -0300

    durante a aula

--- postgres/insert.sql
+++ postgres/insert.sql
@@ -2,3 +2,4 @@ INSERT INTO cliente(id, nome, email) VALUES (72,'Kiko','kiko@org.com');
 INSERT INTO cliente(id, nome, email) VALUES (73,'Chaves','chaves@org.com');
 INSERT INTO cliente(id, nome, email) VALUES (74,'Chiquinha', 'chiquinha@org.com');
 INSERT INTO cliente(id, nome, email) VALUES (75,'Madruga', 'madruga@org.com');
+INSERT INTO cliente(id, nome, email) VALUES (76,'Madruga', 'madruga@org.com');
